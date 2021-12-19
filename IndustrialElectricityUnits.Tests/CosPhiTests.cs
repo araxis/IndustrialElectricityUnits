5547ed5ed987dfae72fce6c6bc4d2a58bfff2a7d
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-19 20:43:57 +0330

    Add project files.

---
+++ IndustrialElectricityUnits.Tests/CosPhiTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace IndustrialElectricityUnits.Tests;
+
+public class CosPhiTests
+{
+    [Fact]
+    public void NotAllowValueLeesThanZero()
+    {
+        var cosPhi = () => new CosPhi(-1);
+        cosPhi.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void NotAllowValueGreaterThanOne()
+    {
+        var construct = () => new CosPhi(2);
+        construct.Should().Throw<ArgumentException>();
+    }
+}
---
+++ IndustrialElectricityUnits/CosPhi.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.GuardClauses;
+
+namespace IndustrialElectricityUnits;
+
+public record CosPhi
+{
+    private readonly double _value;
+
+    public CosPhi(double value)
+    {
+        Value = ValidateValue(value, nameof(value));
+    }
+
+    public double Value
+    {
+        get => _value;
+        init => _value = ValidateValue(value,nameof(Value));
+    }
+
+    public double Phi => Math.Acos(_value);
+    public double SinPhi => Math.Sign(Phi);
+    public double TanPhi => Math.Tan(Phi);
+
+    private double ValidateValue(double value,string paramName)
+    {
+       return Guard.Against.OutOfRange(value, paramName, 0, 1);
+    }
+
+    public static implicit operator CosPhi(double value) => new (value);
+    public static implicit operator double(CosPhi value) => value.Value;
+    public static CosPhi operator -(CosPhi left, CosPhi right) => new(left.Value - right.Value);
+}
