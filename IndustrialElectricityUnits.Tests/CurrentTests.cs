13b07f2507a4cf8678a9bd02709e0be30bd5ee82
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-20 10:50:22 +0330

    Add Current

---
+++ IndustrialElectricityUnits.Tests/CurrentTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace IndustrialElectricityUnits.Tests;
+
+public class CurrentTests
+{
+    [Fact]
+    public void NotAllowNegativeValueInAmpere()
+    {
+        var construct = () => new Ampere(-1);
+
+        construct.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void SumOfTwoCurrentReturnResultInAmpere()
+    {
+        double expectedResultValue = 100;
+        
+        var currentA = 40.A();
+        var currentB = 60.A();
+
+        var result = currentA + currentB;
+       
+        result.Should().BeOfType<Ampere>().Which.Value.Should().Be(expectedResultValue);
+        
+    }
+
+    [Fact]
+    public void DividedByNumberReturnResultInInputCurrentType()
+    {
+        double expectedResultValue = 50;
+        
+        var currentA = 100.A();
+        var operand = 2;
+
+        var result = currentA / operand;
+
+        using var scope = new AssertionScope();
+        result.Should().BeOfType(currentA.GetType());
+        result.Value.Should().Be(expectedResultValue);
+    }
+
+    [Fact]
+    public void MultiplyByNumberReturnResultInInputCurrentType()
+    {
+        double expectedResultValue = 200;
+        
+        var currentA = 100.A();
+        var operand = 2;
+
+        var result = currentA * operand;
+
+        using var scope = new AssertionScope();
+        result.Should().BeOfType(currentA.GetType());
+        result.Value.Should().Be(expectedResultValue);
+    }
+
+    
+ 
+}
---
+++ IndustrialElectricityUnits/Current.cs
@@ -0,0 +1,52 @@
+﻿using Ardalis.GuardClauses;
+
+namespace IndustrialElectricityUnits;
+
+public abstract record  Current
+{
+    private readonly double _value;
+
+    protected Current(double value)
+    {
+        _value = Guard.Against.Negative(value,nameof(Value));
+    }
+
+    public double Value
+    {
+        get => _value;
+        init => _value =   Guard.Against.Negative(value,nameof(Value));
+    }
+
+    public abstract string Unit { get; }
+
+    public abstract Ampere ToAmpere();
+    public abstract Current From(Ampere value);
+
+    public static bool operator > (Current left, Current right) => left.ToAmpere().Value > right.ToAmpere().Value;
+    public static bool operator >= (Current left, Current right) => left.ToAmpere().Value >= right.ToAmpere().Value;
+    public static bool operator < (Current left, Current right) => left.ToAmpere().Value < right.ToAmpere().Value;
+    public static bool operator <= (Current left, Current right) => left.ToAmpere().Value <= right.ToAmpere().Value;
+    public static Ampere operator + (Current left, Current right) => new(left.ToAmpere().Value + right.ToAmpere().Value);
+    public static Current operator / (Current current, double div) => current with { Value = current.Value / div };
+    public static Current operator * (Current current, double div) => current with { Value = current.Value * div };
+
+ 
+
+}
+
+public sealed record Ampere : Current
+{
+    public Ampere(double value) : base(value)
+    {
+    }
+
+
+    public override string Unit => "A";
+    public override Ampere ToAmpere()=> new(Value);
+    public override Ampere From(Ampere value) => new(value.Value);
+
+    public static implicit operator Ampere(double value) => new(value);
+    public static implicit operator double(Ampere value) => value.Value;
+   
+}
+
