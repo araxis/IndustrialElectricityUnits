821b8e88ce8ca5b88857913310004292f500ff80
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-20 10:50:02 +0330

    Add StartMode and CircuitBreakerType

---
+++ IndustrialElectricityUnits/CircuitBreakerType.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.SmartEnum;
+
+namespace IndustrialElectricityUnits;
+
+public sealed class CircuitBreakerType:SmartEnum<CircuitBreakerType> {
+ 
+   public string Description { get; }
+   public static readonly CircuitBreakerType Tmb = new("Tmb", 0,"Thermal Magnetic Breaker");
+   public static readonly CircuitBreakerType Mccb = new("MCCB", 1,"Molded Case Circuit Breaker");
+   public static readonly CircuitBreakerType Acb = new("ACB", 2,"Molded Case Circuit Breaker");
+   public static readonly CircuitBreakerType Fuse = new("FS", 3,"Fuse");
+
+    private CircuitBreakerType(string name, int value, string description) : base(name, value)
+    {
+        Description = description;
+    }
+}
---
+++ IndustrialElectricityUnits/StartMode.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.SmartEnum;
+
+namespace IndustrialElectricityUnits;
+
+public class StartMode:SmartEnum<StartMode>
+{
+    public static readonly StartMode Dol = new("DOL", 1, "Direct Online");
+    public static readonly StartMode Sd = new("SD", 2, "Star-Delta");
+    public static readonly StartMode Ssd = new("DOL", 3, "Soft Start Drive");
+    public static readonly StartMode Vsd = new("DOL", 4, "Variable Speed Drive");
+    public string Description { get; }
+    private StartMode(string name, int value,string description) : base(name, value)
+    {
+        Description = description;
+    }
+}
