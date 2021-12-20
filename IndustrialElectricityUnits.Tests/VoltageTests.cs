b79effddbbc4d1575a45e7b5d87079b21507ddca
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-20 10:51:25 +0330

    Add Voltage

---
+++ IndustrialElectricityUnits.Tests/VoltageTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace IndustrialElectricityUnits.Tests;
+
+public class VoltageTests
+{
+    [Fact]
+    public void NotAllowZeroValue()
+    {
+        var construct = () => new Voltage(0);
+        construct.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void NotAllowNegativeValue()
+    {
+        var contracts = () => new Voltage(-1);
+        contracts.Should().Throw<ArgumentException>();
+    }
+}
---
+++ IndustrialElectricityUnits/Voltage.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.GuardClauses;
+
+namespace IndustrialElectricityUnits;
+
+public record Voltage
+{
+    private readonly double _value;
+
+    public Voltage(double value)
+    {
+        _value = ValueValidation(value,nameof(value));
+    }
+
+  
+
+    public double Value
+    {
+        get => _value;
+        init => _value =  ValueValidation(value,nameof(Value));
+    }
+
+    public static implicit operator Voltage(double value) => new(value);
+    public static implicit operator double(Voltage value) => value.Value;
+
+    private double ValueValidation(double value,string paramName)
+    {
+        return Guard.Against.NegativeOrZero(value,paramName);
+    }
+   
+
+}
+
+
