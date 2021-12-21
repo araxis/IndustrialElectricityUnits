fe3eecf3aec30120598d892b4d9148d220dae836
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-21 11:41:04 +0330

    Fix StartMode wrong names.

--- IndustrialElectricityUnits/StartMode.cs
+++ IndustrialElectricityUnits/StartMode.cs
@@ -4,13 +4,14 @@ namespace IndustrialElectricityUnits;
 
 public class StartMode:SmartEnum<StartMode>
 {
-    public static readonly StartMode Dol = new("DOL", 1, "Direct Online");
-    public static readonly StartMode Sd = new("SD", 2, "Star-Delta");
-    public static readonly StartMode Ssd = new("DOL", 3, "Soft Start Drive");
-    public static readonly StartMode Vsd = new("DOL", 4, "Variable Speed Drive");
+    public static readonly StartMode Dol = new("DOL", 0, "Direct Online");
+    public static readonly StartMode Sd = new("SD", 1, "Star-Delta");
+    public static readonly StartMode Ssd = new("SSD", 2, "Soft Start Drive");
+    public static readonly StartMode Vsd = new("VSD", 3, "Variable Speed Drive");
     public string Description { get; }
     private StartMode(string name, int value,string description) : base(name, value)
     {
         Description = description;
+        
     }
 }
