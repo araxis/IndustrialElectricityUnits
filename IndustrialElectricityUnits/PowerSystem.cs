accaa4110bf6f1cfe9c1cdb76b69493ac276e1cd
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-21 11:41:21 +0330

    add PowerSystem.

---
+++ IndustrialElectricityUnits/PowerSystem.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.SmartEnum;
+
+namespace IndustrialElectricityUnits;
+
+public sealed class PowerSystem:SmartEnum<PowerSystem>
+{
+   // SinglePhase,ThreePhase
+   public static readonly PowerSystem SinglePhase = new("Single Phase", 0);
+   public static readonly PowerSystem TwoPhase = new("Two Phase", 1);
+   public static readonly PowerSystem ThreePhase = new("Three Phase", 2);
+
+   private PowerSystem(string name, int value) : base(name, value)
+    {
+    }
+
+  
+}
